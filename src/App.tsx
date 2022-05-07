7e0e1acad52f97b0cd24b9c3a2cf0918c90d4306
Miguel Rodríguez <miguelrodrii@outlook.com>
2022-05-07 17:00:21 -0500

    modified general dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import React from "react";
-
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+    return <h1>hi</h1>;
 }
 
 export default App;
